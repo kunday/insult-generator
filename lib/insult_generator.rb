c922ecb2f9dc613936105cb9646682df5573d4de
Travis Hooper <travis.d.hooper@gmail.com>
2020-05-20 18:00:33 -0700

    Fixes data load from outside gem dir

--- lib/insult_generator.rb
+++ lib/insult_generator.rb
@@ -2,7 +2,8 @@ require 'yaml'
 
 class InsultGenerator
   def self.random_insult
-    data = YAML.load_file(File.expand_path("lib/data/insults.yml"))
+    data_file = File.join(File.dirname(__FILE__), "data/insults.yml")
+    data = YAML.load_file(data_file)
     column1_rand = data["column1"][rand(50)]
     column2_rand = data["column2"][rand(50)]
     column3_rand = data["column3"][rand(50)]
